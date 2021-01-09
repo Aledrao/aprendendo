cd0ac5590b1cdfde341e13ce4611e90be5e6331d
Alex souza <aalex_sandro@hotmail.com>
2021-01-08 23:32:24 -0300

    Finalizacao de particulas e configuracao inicial das cameras

--- inimigo_quadrado_h.gd
+++ inimigo_quadrado_h.gd
@@ -2,10 +2,13 @@ extends StaticBody
 
 const VELOCIDADE = 6
 const DISTANCIA = 12
+const TIME_PERIOD = 1
 
 var posicao_inicial
 var posicao_final
 var flip = true
+var time = 0
+var dealth = false
 
 var t;
 
@@ -15,6 +18,11 @@ func _ready():
 	posicao_final = posicao_inicial + DISTANCIA
 	
 func _process(delta):
+	if(dealth):
+		time += delta
+	else:
+		time = 0
+		
 	if(posicao_inicial <= posicao_final and flip):
 		t.origin += Vector3(delta * VELOCIDADE, 0, 0)
 		set_transform(t)
@@ -26,6 +34,12 @@ func _process(delta):
 		set_transform(t)
 		if(t.origin.x <= posicao_inicial):
 			flip = true
-			
+
+	if(dealth and time > TIME_PERIOD):
+		$".".queue_free()
+
 func dano():
-	$".".queue_free()
+	$MeshInstance.visible = false
+	$CollisionShape.disabled = true
+	$Particles.emitting = true
+	dealth = true
